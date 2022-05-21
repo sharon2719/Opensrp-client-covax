95efe9a9f48dc253c61e5fb3ca3a8799d47f4d5d
sharon2719 <otiennosharon@gmail.com>
2022-05-21 09:21:11 +0300

    first commit

---
+++ app/src/main/java/com/example/opensrp_client_covax/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.opensrp_client_covax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
