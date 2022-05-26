bb537dd82f61e6fb83a1e2724b67e72c5341189e
sharon2719 <otiennosharon@gmail.com>
2022-05-26 19:15:32 +0300

    login-implementation WIP

---
+++ app/src/main/java/com/example/opensrp_client_covax/fragment/AdvancedSearchFragment.java
@@ -0,0 +1,2 @@
+package com.example.opensrp_client_covax.fragment;public class AdvancedSearchFragment {
+}
---
+++ app/src/main/java/com/example/opensrp_client_covax/fragment/ChildRegisterFragment.java
@@ -0,0 +1,49 @@
+package com.example.opensrp_client_covax.fragment;
+
+import android.view.View;
+
+import org.smartregister.view.fragment.BaseRegisterFragment;
+
+import java.util.HashMap;
+
+public class ChildRegisterFragment extends BaseRegisterFragment {
+    @Override
+    protected void initializePresenter() {
+
+    }
+
+    @Override
+    public void setUniqueID(String qrCode) {
+
+    }
+
+    @Override
+    public void setAdvancedSearchFormData(HashMap<String, String> advancedSearchFormData) {
+
+    }
+
+    @Override
+    protected String getMainCondition() {
+        return null;
+    }
+
+    @Override
+    protected String getDefaultSortQuery() {
+        return null;
+    }
+
+    @Override
+    protected void startRegistration() {
+
+    }
+
+    @Override
+    protected void onViewClicked(View view) {
+
+    }
+
+    @Override
+    public void showNotFoundPopup(String opensrpId) {
+
+    }
+}
